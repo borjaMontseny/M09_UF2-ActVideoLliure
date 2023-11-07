b5cb6a43ee7ba893118e8756f50b594eff684103
Borja Montseny <68342939+borjaMontseny@users.noreply.github.com>
2023-11-07 14:46:02 +0100

    ...

--- js/script.js
+++ js/script.js
@@ -1,20 +1,15 @@
-// Selecciona el elemento switchMode
 var switchMode = document.getElementById("switchMode");
 
-// Añade un evento de clic al elemento switchMode
 switchMode.addEventListener("click", cambiarModo);
 
-// Define la función para cambiar el modo
 function cambiarModo() {
-    // Comprueba el color de fondo actual del body
     var colorDeFondo = document.body.style.backgroundColor;
 
-    // Cambia el color de fondo y el emoji dependiendo del color actual
-    if (colorDeFondo === "rgb(80, 80, 80)") { // #505050 en formato rgb
+    if (colorDeFondo === "rgb(16, 16, 16)") { // #101010 en format rgb
         document.body.style.backgroundColor = "white";
         switchMode.textContent = "🌙";
     } else {
-        document.body.style.backgroundColor = "#505050";
+        document.body.style.backgroundColor = "#101010";
         switchMode.textContent = "☀️";
     }
 }
